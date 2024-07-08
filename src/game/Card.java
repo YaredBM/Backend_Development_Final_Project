7a77e53ce7ae06a77e711e3b3fc0a5003edb34fb
Alune69 <jonathancmatas@gmail.com>
2024-07-08 20:16:07 +0200

    Update Card.java

--- src/game/Card.java
+++ src/game/Card.java
@@ -1,6 +1,10 @@
 package game;
 
-public class Card {
+import java.io.Serializable;
+
+public class Card implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
     private String suit;
     private int rank;
 
