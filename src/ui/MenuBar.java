1bfdf764819e25bd84eb89687a9c4ffc40ef4b79
Yáred Iessé Bustillo Medina <92419630+YaredBM@users.noreply.github.com>
2024-07-08 20:08:06 +0200

    Update MenuBar.java

--- src/ui/MenuBar.java
+++ src/ui/MenuBar.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuBar extends JMenuBar {
     public MenuBar(GameController controller) {
@@ -12,9 +13,36 @@ public class MenuBar extends JMenuBar {
         JMenuItem openGame = new JMenuItem("Open Game");
         openGame.addActionListener(e -> controller.openGame());
 
+        JMenuItem about = new JMenuItem("About");
+        about.addActionListener(e -> showAboutDialog());
+
         menu.add(newGame);
         menu.add(saveGame);
         menu.add(openGame);
+        menu.add(about);
         add(menu);
+    }
+
+    private void showAboutDialog() {
+        JDialog aboutDialog = new JDialog();
+        aboutDialog.setTitle("About");
+        aboutDialog.setSize(400, 300);
+        aboutDialog.setLayout(new GridLayout(6, 1));
+
+        String[] names = {
+            "Yared Iesse Bustillo Medina",
+            "Jonathan David Concha Matas",
+            "Josue David Pavon Maldonado",
+            "Julia Correira Bindi",
+            "Bruna Perry Pierobon",
+            "Sagar Bhusal"
+        };
+
+        for (String name : names) {
+            JLabel label = new JLabel(name, SwingConstants.CENTER);
+            aboutDialog.add(label);
+        }
+
+        aboutDialog.setVisible(true);
     }
 }
