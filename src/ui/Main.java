0a1bc3d691b22f4359c6ebc73653e837ccbd54cd
jpavon-jp <josuepavon2004@gmail.com>
2024-07-10 23:51:15 +0200

    Main.java

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,7 +1,8 @@
 package ui;
 
+// Create the Main class which is the entry point of the application. 
 public class Main {
     public static void main(String[] args) {
-        new GameController();
+        new GameController(); // Creates a new instance of GameController, which starts the game.
     }
 }
