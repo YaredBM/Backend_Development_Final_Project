72e03d6cb5a389ab934cb92c5c2fc2d6f31d481f
Yáred Iessé Bustillo Medina <92419630+YaredBM@users.noreply.github.com>
2024-07-10 14:38:14 +0200

    Update Deck.java

--- src/game/Deck.java
+++ src/game/Deck.java
@@ -4,10 +4,21 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
+/**
+ * Manages a deck of cards, including shuffling and dealing cards.
+ */
 public class Deck implements Serializable {
-    private List<Card> cards;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<Card> cards;
 
+    /**
+     * Constructor to initialize the deck with cards.
+     */
     public Deck() {
         cards = new ArrayList<>();
         String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
@@ -22,10 +33,25 @@ public class Deck implements Serializable {
         }
     }
 
-    public void shuffle() {
-        Collections.shuffle(cards);
+    public List<Card> getCards() {
+        return cards;
     }
 
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    /**
+     * Shuffles the deck using a random number generator.
+     */
+    public void shuffle() {
+        Collections.shuffle(cards, new Random());
+    }
+
+    /**
+     * Deals a card from the top of the deck.
+     * @return The dealt card.
+     */
     public Card dealCard() {
         if (!cards.isEmpty()) {
             return cards.remove(0);
@@ -33,11 +59,28 @@ public class Deck implements Serializable {
         return null;
     }
 
+    /**
+     * Checks if the deck has cards remaining.
+     * @return true if the deck has cards, false otherwise.
+     */
     public boolean hasCards() {
         return !cards.isEmpty();
     }
 
+    /**
+     * Returns the number of cards in the deck.
+     * @return The size of the deck.
+     */
     public int size() {
         return cards.size();
+    }
+
+    /**
+     * Prints information about the deck.
+     */
+    public void printInfo() {
+        for (Card card : cards) {
+            card.printInfo();
+        }
     }
 }
